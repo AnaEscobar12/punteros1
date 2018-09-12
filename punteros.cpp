4de36a695b61bb55474bcf03d199520f26c80888
Ana Karen Escobar Casiano <carlasegundo11@gmail.com>
2018-09-12 08:31:30 -0500

    first commit

---
+++ punteros.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+struct Dato{
+	int dato1;
+	char dato2[20];
+};
+
+int main(){
+	struct Dato Var1;
+    struct Dato *Ptr;
+    Ptr=&Var1;
+	cout<<"Puntero: "<<&Ptr<<endl;//Imprime el valor del puntero
+	cout<<"Direccion del puntero: "<<Ptr<<endl;//Imprime la direccion del puntero
+	cout<<"Ingresa el numero del puntero";
+	cin>>Ptr->dato1;
+	cout<<"Valor ingresado: "<<Ptr->dato1<<"\n";
+	cin>>Ptr->dato2;
+	
+
+	return 0;
+}
